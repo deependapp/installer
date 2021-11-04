b5463522b1b9c6b029d3957a67f010f9e0031913
Waviest <31796727+WaviestBalloon@users.noreply.github.com>
2021-11-04 17:37:48 +0000

    Update installdeepend.sh

--- installdeepend.sh
+++ installdeepend.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 echo "This bash script will install the lastest version of Deepend from the GitHub repo: "
 echo "   https://github.com/deependapp/client"
-echo "Make sure you have Tauri installed; if you don't this installer will fail!"
+echo "Make sure you have Tauri and curl installed; if you don't this installer will fail!"
 echo ""
 echo "Do you wish to proceed with installation? y/n"
 
 read conf
 if [ $conf == "y" ] then
-
+echo "Creating installation folder..."
+mkdir Deepend
+echo "Downloading Deepend source..."
+curl -o 
 else
 echo "The installation has been aborted due to user input; please run the installation script again if you wish to install"
 fi
